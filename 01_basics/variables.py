ca2aec0b7276e4361576127270df831277a5826f
Zeeshan Haider <zeeshan9402529@gmail.com>
2026-07-27 13:05:09 +0500

    Add variables, data types, and type casting examples

---
+++ 01_basics/variables.py
@@ -0,0 +1,21 @@
+name = "Zeeshan"
+age = 25
+height = 5.9
+is_employed = False
+
+print(name, age, height, is_employed)
+print(type(name), type(age), type(height), type(is_employed))
+
+x = y = z = 0
+print(x, y, z)
+
+a, b, c = "Python", 3.10, True
+print(a, b, c)
+
+age_str = str(age)
+height_int = int(height)
+flag = bool(0)
+
+print(age_str, type(age_str))
+print(height_int, type(height_int))
+print(flag, type(flag))
