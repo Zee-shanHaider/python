b9248d57b79d892b5ab3dce35c096ff3db37dab6
Zeeshan Haider <zeeshan9402529@gmail.com>
2026-07-27 13:07:03 +0500

    Add data types and string operations

---
+++ 01_basics/data_types.py
@@ -0,0 +1,24 @@
+integer = 10
+floating = 3.14
+string = "hello"
+boolean = True
+nothing = None
+
+print(type(integer))
+print(type(floating))
+print(type(string))
+print(type(boolean))
+print(type(nothing))
+
+text = "Python"
+print(text[0])
+print(text[-1])
+print(text[0:3])
+print(text.upper())
+print(text.lower())
+print(len(text))
+
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(2 ** 8)
